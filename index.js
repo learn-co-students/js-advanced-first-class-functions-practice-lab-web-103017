360caf396fb25abc6cea6273bf28795722508aaa
github username <github email address>
2017-12-12 11:36:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames(drivers) {
+  drivers.forEach(function (driver){console.log(driver.name)})
+};
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function (driver){
+    if (driver.hometown == location) {
+      console.log(driver.name);
+    };
+  });
+};
+
+function driversByRevenue(driver) {
+  let arr = [...driver]
+  arr.sort(function(a,b){return a.revenue - b.revenue})
+  return arr
+};
+
+function driversByName(driver) {
+  let arr = [...driver]
+  return arr.sort(function(a,b){return a.name.localeCompare(b.name)})
+};
+
+function totalRevenue(driver) {
+  let arr = []
+  driver.forEach(function (driver) {
+    arr.push(driver.revenue)
+  })
+  return arr.reduce(function(a,b){return a + b
+  })
+};
+
+function averageRevenue(driver) {
+  return totalRevenue(driver) / driver.length
+};
